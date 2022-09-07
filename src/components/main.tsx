0ba04ee6a60487372632132a98e8624bb3be92f1
Anton Klock <klock.anton@gmail.com>
2022-09-07 12:00:54 +0200

    added third view

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -3,15 +3,17 @@ import "../scss/components/main.scss";
 import ProjectsView from "../components/views/ProjectsView";
 import SettingsView from "./views/SettingsView";
 import Menu from "./Menu";
+import ThirdView from "./views/ThirdView";
 
 const Main = () => {
-  const [view, setView] = useState("projects");
+  const [view, setView] = useState("Projects");
   return (
     <div className="main">
       <Menu setView={setView} />
       <div className="views">
         {view === "Projects" && <ProjectsView />}
         {view === "Settings" && <SettingsView />}
+        {view === "Third view" && <ThirdView />}
       </div>
     </div>
   );
---
+++ src/components/views/ThirdView.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../../scss/components/views/thirdView.scss";
+
+const ThirdView = () => {
+  return <div className="thirdView">Third View</div>;
+};
+
+export default ThirdView;
