3dd94b21cc7be8852479d1f28bb162a2f8fbdc32
Anton Klock <anton@klockworks.se>
2022-09-08 18:18:30 +0200

    All .tsx classes now begin with a capital letter

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "../scss/components/main.scss";
-import ProjectsView from "../components/views/ProjectsView";
+import ProjectsView from "./views/ProjectsView";
 import SettingsView from "./views/SettingsView";
 import Menu from "./Menu";
 import ThirdView from "./views/ThirdView";
